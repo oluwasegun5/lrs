53ecf9267dc05e8b660adc05030ce5a597022644
localhost <Adeayosegun5@gmail.com>
2025-10-22 11:02:59 +0100

    add AccountDto and VerbDto classes for account and verb data representation

--- src/main/java/africa/enumverse/lrs/dto/StatementRequest.java
+++ src/main/java/africa/enumverse/lrs/dto/StatementRequest.java
@@ -5,9 +5,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-import java.util.Map;
-
 @Data
 @Builder
 @NoArgsConstructor
@@ -19,98 +16,4 @@ public class StatementRequest {
     private ActivityDto object;
     private ContextDto context;
     private ResultDto result;
-
-    @Data
-    @Builder
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class ActorDto {
-        private String id; // UUID
-        private String name;
-        private String mbox; // mailto: URI
-        private String mboxSha1sum;
-        private String openId;
-        private AccountDto account;
-        private String objectType; // Agent | Group
-
-        @Data
-        @Builder
-        @NoArgsConstructor
-        @AllArgsConstructor
-        public static class AccountDto {
-            private String name;
-            private String homePage;
-        }
-    }
-
-    @Data
-    @Builder
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class VerbDto {
-        private String id; // URI
-        private Map<String, String> display; // language map
-    }
-
-    @Data
-    @Builder
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class ActivityDto {
-        private String id; // URI
-        private String objectType; // Activity|Agent|Group|SubStatement|StatementRef
-        private ActivityDefinitionDto definition;
-    }
-
-    @Data
-    @Builder
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class ActivityDefinitionDto {
-        private Map<String, String> name; // language map
-        private Map<String, String> description;
-        private String type; // URI
-        private String moreInfo; // URI
-        private Map<String, Object> extensions;
-    }
-
-    @Data
-    @Builder
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class ContextDto {
-        private String registration;
-        private String instructorId;
-        private String teamId;
-        private Map<String, List<ActivityDto>> contextActivities;
-        private String revision;
-        private String platform;
-        private String language;
-        private String statement; // statement id
-        private Map<String, Object> extensions;
-    }
-
-    @Data
-    @Builder
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class ResultDto {
-        private ScoreDto score;
-        private Boolean success;
-        private Boolean completion;
-        private String response;
-        private String duration; // ISO8601
-        private Map<String, Object> extensions;
-
-        @Data
-        @Builder
-        @NoArgsConstructor
-        @AllArgsConstructor
-        public static class ScoreDto {
-            private Float scaled;
-            private Float raw;
-            private Float min;
-            private Float max;
-        }
-    }
 }
