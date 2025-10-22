1ac34207294871f46b1e4e7b5dca1217d4e8fbd1
localhost <Adeayosegun5@gmail.com>
2025-10-22 10:28:28 +0100

    add ContextResponse class to represent context data in API responses

---
+++ src/main/java/africa/enumverse/lrs/dto/AccountResponse.java
@@ -0,0 +1,16 @@
+package africa.enumverse.lrs.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AccountResponse {
+    private String name;
+    private String homePage;
+}
+
