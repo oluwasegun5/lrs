cce046ba6a892b04643358fffa8cf496b154e80b
localhost <Adeayosegun5@gmail.com>
2025-10-24 18:02:34 +0100

    add ActivityDefinitionDto class for activity definition data representation

---
+++ src/main/java/africa/enumverse/lrs/config/MongoConfig.java
@@ -0,0 +1,29 @@
+package africa.enumverse.lrs.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.convert.DbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+
+@Configuration
+public class MongoConfig {
+
+    @Bean
+    public MappingMongoConverter mappingMongoConverter(MongoDatabaseFactory factory, MongoMappingContext context) {
+        DbRefResolver dbRefResolver = new DefaultDbRefResolver(factory);
+        MappingMongoConverter mappingConverter = new MappingMongoConverter(dbRefResolver, context);
+
+        // Remove _class field from documents
+        mappingConverter.setTypeMapper(new DefaultMongoTypeMapper(null));
+
+        // Configure dot replacement for map keys
+        // This replaces dots (.) with a custom character to avoid MongoDB restrictions
+        mappingConverter.setMapKeyDotReplacement("_DOT_");
+
+        return mappingConverter;
+    }
+}
