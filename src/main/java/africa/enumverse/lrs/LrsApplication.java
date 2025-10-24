90e2a8f6df49e6277da9b9384ca4a2f84970bb7e
localhost <Adeayosegun5@gmail.com>
2025-10-24 18:02:46 +0100

    add ActivityDefinitionDto class for activity definition data representation

--- src/main/java/africa/enumverse/lrs/LrsApplication.java
+++ src/main/java/africa/enumverse/lrs/LrsApplication.java
@@ -2,8 +2,10 @@ package africa.enumverse.lrs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class LrsApplication {
 
     public static void main(String[] args) {
